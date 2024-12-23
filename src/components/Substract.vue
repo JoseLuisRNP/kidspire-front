57b86b8d352b542964ac31a59ab26f44d82e9038
joseluisrnp <reciojoselu@gmail.com>
2024-12-23 12:03:15 +0100

    fix: correct visibility logic for digit display in Substract component

--- src/components/Substract.vue
+++ src/components/Substract.vue
@@ -256,7 +256,7 @@ function clearUserAnswer() {
                     :key="'num1-' + index"
                     class="w-12 text-center"
                 >
-                <span v-show="d(digit !== '0' || digit === '0' && index !== 0) || num1.toString().length == 1 ">{{ digit }}</span>
+                <span v-show="(digit !== '0' || digit === '0' && index !== 0) || num1.toString().length == 1 ">{{ digit }}</span>
                 </div>
             </div>
             <div class="absolute -left-2 w-12 text-center">
